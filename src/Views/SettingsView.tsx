9201fb1d0da5e40890e4cb616d2d8c4c9547be28
gcannata <gcannata@gmail.com>
2025-06-22 19:33:12 +0200

    feat: Change button variant to secondary for improved UI consistency in SettingsView

--- src/Views/SettingsView.tsx
+++ src/Views/SettingsView.tsx
@@ -542,7 +542,7 @@ export function SettingsView() {
             {/* Fixed Bottom Actions */}
             <div className="fixed bottom-0 left-0 right-0 bg-background border-t border-border p-4">
                 <div className="max-w-4xl mx-auto flex justify-between">
-                    <Button variant="outline" onClick={() => commandManager.executeCommand('view.close')}>
+                    <Button variant="secondary" onClick={() => commandManager.executeCommand('view.close')}>
                         <XIcon className="h-4 w-4 mr-2" />
                         Close
                     </Button>
